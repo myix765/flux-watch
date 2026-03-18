556d2fd0d6e41e5fb069bf9454e96c019f5146dd
myix765 <myix765@gmail.com>
2026-03-17 22:59:59 -0700

    add header

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,4 +2,11 @@
     import AppleWatch from '$lib/assets/apple-watch.png';
 </script>
 
-<img src={AppleWatch} alt="Apple Watch" class="h-3/5" />
+<div class="h-screen flex gap-x-24 *:flex *:justify-center-safe *:items-center-safe">
+    <div class="w-1/4 p-24">
+        <h1 class="font-anta text-[5rem] leading-[1.2]">Apple Watch Flux Face Recreation</h1>
+    </div>
+    <div class="flex-1 h-full">
+        <img src={AppleWatch} alt="Apple Watch" class="h-2/3" />
+    </div>
+</div>
