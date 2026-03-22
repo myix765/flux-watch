a210b5fff04694189715cf79fe29aa06ce5e601e
myix765 <myix765@gmail.com>
2026-03-21 21:45:42 -0400

    add pixijs canvas

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import AppleWatch from '$lib/assets/apple-watch.png';
+    import Clock from './Clock.svelte';
 </script>
 
-<div class="h-screen flex gap-x-24 *:flex *:justify-center-safe *:items-center-safe">
+<div class="flex h-screen gap-x-24 *:flex *:items-center-safe *:justify-center-safe">
     <div class="w-1/4 p-24">
         <h1 class="font-anta text-[5rem] leading-[1.2]">Apple Watch Flux Face Recreation</h1>
     </div>
-    <div class="flex-1 h-full">
+    <div class="h-full flex-1">
         <img src={AppleWatch} alt="Apple Watch" class="h-2/3" />
+        <Clock />
     </div>
 </div>
---
+++ ui/src/routes/Clock.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import * as PIXI from 'pixi.js';
+    import { onMount } from 'svelte';
+
+    let canvas: HTMLCanvasElement;
+    let container: HTMLDivElement;
+
+    onMount(async () => {
+        const app = new PIXI.Application();
+
+        await app.init({
+            canvas,
+            backgroundColor: 0x000,
+            resizeTo: container,
+        });
+    })
+</script>
+
+<div bind:this={container} class="absolute mr-[0.94rem] w-[14.8%] h-[33%] rounded-[2.5rem] overflow-hidden">
+    <canvas bind:this={canvas}></canvas>
+</div>
