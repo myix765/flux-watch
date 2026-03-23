be667b5fb184336466d739a22c6292be7ffe9e6b
myix765 <myix765@gmail.com>
2026-03-22 23:16:06 -0400

    load and parse font rust

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,23 @@
 use wasm_bindgen::prelude::*;
+use ttf_parser::Face;
+use web_sys::console;
+
+macro_rules! log {
+    ($($t:tt)*) => {
+        console::log_1(&format!($($t)*).into())
+    }
+}
+
+const FONT_DATA: &[u8] = include_bytes!("../assets/Anta/Anta-Regular.ttf");
 
 #[wasm_bindgen]
-pub fn return_num() -> u32 {
-    12
+pub fn get_glyph(d: char) {
+    let face = Face::parse(FONT_DATA, 0).expect("Failed to parse font");
+    for name in face.names() {
+        if name.name_id == ttf_parser::name_id::FAMILY {
+            if let Some(s) = name.to_string() {
+                log!("Family: {}", s);
+            }
+        }
+    }
 }
--- ui/src/routes/Clock.svelte
+++ ui/src/routes/Clock.svelte
@@ -1,32 +1,31 @@
 <script lang="ts">
-  import * as PIXI from 'pixi.js';
-  import { onMount } from 'svelte';
+    import * as PIXI from 'pixi.js';
+    import { onMount } from 'svelte';
 
-  let canvas: HTMLCanvasElement;
-  let container: HTMLDivElement;
+    let canvas: HTMLCanvasElement;
+    let container: HTMLDivElement;
 
-  onMount(async () => {
-    const { default: init, return_num } = await import('$lib/wasmc/flux_core.js');
-    await init();
-    console.log(return_num());
+    onMount(async () => {
+        const { default: init, get_glyph } = await import('$lib/wasmc/flux_core.js');
+        await init();
 
-    const PADDING = 16;
-    const app = new PIXI.Application();
+        const PADDING = 16;
+        const app = new PIXI.Application();
 
-    await app.init({
-      canvas,
-      backgroundColor: 0x1099bb,
-      resizeTo: container
+        await app.init({
+            canvas,
+            backgroundColor: 0x1099bb,
+            resizeTo: container
+        });
+
+        app.stage.x = PADDING;
+        app.stage.y = PADDING;
     });
-
-    app.stage.x = PADDING;
-    app.stage.y = PADDING;
-  });
 </script>
 
 <div
     bind:this={container}
-    class="absolute top-[25.3%] bottom-[25.3%] left-[12.4%] right-[17%] overflow-hidden rounded-[15.75%] opacity-55"
+    class="absolute top-[25.3%] right-[17%] bottom-[25.3%] left-[12.4%] overflow-hidden rounded-[15.75%] opacity-55"
 >
     <canvas bind:this={canvas}></canvas>
 </div>
